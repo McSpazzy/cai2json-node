552f9e8ce1c271565999347d5833227e29cf0233
SuperSpazzy <spazzymcspazzington@gmail.com>
2023-07-15 08:47:57 +1000

    Initial

---
+++ README.md
@@ -0,0 +1,24 @@
+# cai2json
+
+## Installation
+
+Install globally:
+
+```sh
+npm install -g cai2json
+```
+
+## Usage
+
+Convert cai to json file
+
+```sh
+cai2json filein.cai fileout.json
+```
+
+Convert json to cai file. Currently in the works. 
+
+```sh
+json2cai filein.json fileout.cai
+```
+
---
+++ src/bin/cai2json.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import { CombinedActorInfo } from 'combined-actor-info';
+import fs from 'fs';
+
+const [, , ...args] = process.argv;
+
+const fileIn = args[0];
+const fileOut = args.length > 1 ? args[1] : undefined;
+
+function load(fileIn: string): CombinedActorInfo | undefined {
+    if (!fs.existsSync(fileIn)) {
+        console.log('File does not exist', fileIn);
+        return;
+    }
+    try {
+        var data = fs.readFileSync(fileIn);
+        var entry = CombinedActorInfo.FromArrayBuffer(data.buffer);
+        return entry;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+var actor = load(fileIn);
+if (actor) {
+    if (!fileOut) {
+        console.log(JSON.stringify(actor, null, 4))
+    } else {
+        try {
+            fs.writeFileSync(fileOut, JSON.stringify(actor, null, 4));
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
